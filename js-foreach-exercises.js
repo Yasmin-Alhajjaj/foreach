703078e5a6854c38b7176c27a45a9f6b20a4cb5c
Yasmin-Alhajjaj <yasmin.alhajjaj@gmail.com>
2019-07-18 13:10:57 +0300

    sol

--- js-foreach-exercises.js
+++ js-foreach-exercises.js
@@ -20,7 +20,23 @@ function doubleValues(arr){
   arr.forEach(element => x.push(element*2));
   return x;
 }
-console.log(doubleValues([1,2,3]))
+//console.log(doubleValues([1,2,3]))
+
+function doubleValues(arr){
+  var x=[];
+   arr.forEach(function(element, index, array ){
+
+    array[index]=element*2
+   // return index;
+
+   } );
+   return arr;
+ }
+ console.log(doubleValues([1,2,3]))
+
+
+
+
 /*
 * Exercise 2:
 * Write a function called onlyEvenValues which 
@@ -43,6 +59,18 @@ function onlyEvenValues(arr){
 }
 console.log(onlyEvenValues([5,1,2,3,10]))
 
+/* function onlyEvenValues(arr){
+
+  arr.forEach((element,index,array) => {if(element%2!==0) 
+   { array[index]=element}
+  
+  })
+    return arr
+}
+console.log(onlyEvenValues([5,1,2,3,10]))
+ */
+
+
 /*
 * Exercise 3:
 * Write a function called showFirstAndLast which accepts
@@ -60,10 +88,11 @@ console.log(onlyEvenValues([5,1,2,3,10]))
 */
 function showFirstAndLast(arr){
 var x=[];
-arr.forEach( (element) => {x.push(element.slice(0,1)+element.slice(-1) )} )
+arr.forEach( (element, index) => 
+{x.push(element.slice(0,1)+element.slice(-1) )} )
   return x;
 }
-console.log(showFirstAndLast(['colt','matt','tim','udemy']))
+//console.log(showFirstAndLast(['colt','matt','tim','udemy']))
 
 /*
 * Exercise 4:
@@ -78,18 +107,24 @@ console.log(showFirstAndLast(['colt','matt','tim','udemy']))
 *   [{name: 'Elie', title:'instructor'}, {name: 'Tim', title:'instructor'}, {name: 'Matt', title:'instructor'}, {name: 'Colt', title:'instructor'}]
 *
 */
+
 function addKeyAndValue(arr,key,value){
- var x=[];
- arr.forEach( (element) => {x.push(element+(arr[key]= value)) } ) 
- return x; 
+
+ arr.forEach((element,index)=> {
+   element[key]=value;
+ })
+ return arr
 }
 console.log(addKeyAndValue([{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}], 'title', 'instructor') )
 /*
 * Exercise 5:
-* Write a function called vowelCount which accepts a string and returns 
+* Write a function called vowelCount which accepts a
+ string and returns 
 an object with the keys as the vowel and the 
-* values as the number of times the vowel appears in the string.
- This function should be case insensitive so a lowercase letter and 
+* values as the number of times the vowel appears
+ in the string.
+ This function should be case insensitive so a
+  lowercase letter and 
  uppercase letter should count
 * 
 * Test Cases 1: vowelCount('Elie') // {e:2,i:1};
@@ -111,4 +146,46 @@ an object with the keys as the vowel and the
 
 function vowelCount(str){
   //Your Code Here
+  obj={}
+  // var i=0
+  // var e=0
+  // var o=0
+  // var a=0
+  // var u=0
+  // str = str.toLowerCase();
+  // str = str.split("");
+  // str.forEach(element => { 
+   
+  //   if (element==='i') 
+  //     obj[element] =++i;
+
+  //     if (element==='e') 
+  //     obj[element]=++e;
+
+  //     if (element==='o') 
+  //     obj[element]=++o;
+
+  //     if (element==='a') 
+  //     obj[element]=++a;
+
+  //     if (element==='u') 
+  //     obj[element]=++u;
+      
+  // })
+  str = str.toLowerCase();
+  str = str.split("");
+  var counter = 0;
+  str.forEach(element => {
+    counter =0;
+    if(element ==='a' || 'o' || 'u' || 'i' || 'e'){
+      str.forEach(compare => {
+        if(element === compare){
+          counter++;
+          obj[element] = counter;
+        }
+      })
+    }
+  })
+return obj;
 }
+console.log(vowelCount('Eliiieouaaouaaou'));
